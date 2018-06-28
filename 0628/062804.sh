94ae042ec8f30fedef8389562d58f0750d4e0cbe
s18001 <s18001@std.it-college.ac.jp>
2018-06-28 02:17:03 +0000

    kadai

--- 0628/062804.sh
+++ 0628/062804.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-#cp ~/unix.2018/personal_information.csv file1
-
-cut -d , -f 7 file1 | sort | uniq -c | sort -n | tac | head -n 5 
+cut -d , -f 7  personal_information.csv | sort | uniq -c | sort -n | tac | head -n 5 
 
